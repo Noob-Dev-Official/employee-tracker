a88940639af813de9ed129279877bcc643b51f50
Muntasir <muntasirenamalhuda@gmail.com>
2021-08-14 17:55:36 +0100

    done initial setup of the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import { AuthProvider } from './contexts/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
+import ForgotPassword from './pages/ForgotPassword';
+import UpdateProfile from './pages/UpdateProfile';
+
+import './App.scss';
 
 function App() {
 	return (
 		<>
-			<h1>employee tracker</h1>
+			<Router>
+				<div className='auth-app'>
+					<div className='content'>
+						<AuthProvider>
+							<Switch>
+								<PrivateRoute exact path='/' component={Home} />
+								<PrivateRoute
+									exact
+									path='/update-profile'
+									component={UpdateProfile}
+								/>
+								<Route exact path='/sign-in'>
+									<SignIn />
+								</Route>
+								<Route exact path='/sign-up'>
+									<SignUp />
+								</Route>
+								<Route exact path='/forgot-password'>
+									<ForgotPassword />
+								</Route>
+							</Switch>
+						</AuthProvider>
+					</div>
+				</div>
+			</Router>
 		</>
 	);
 }
