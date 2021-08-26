166089057930257e883949e516db4b6a49aff63a
Muntasir2001 <muntasirenamalhuda@gmail.com>
2021-08-26 12:46:33 +0100

    fixed layout margin for different screen sizes

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,44 +3,43 @@ import React from 'react';
 import styled from 'styled-components';
 
 const CardParent = styled.div`
-    display: flex;
-    flex-direction: ${({ isSmallMode }) => (isSmallMode ? 'row' : 'column')};
-    background-color: #bbe1fa;
-    min-width: 200px;
-    border-radius: 10px;
-    cursor: pointer;
+	display: flex;
+	flex-direction: ${({ isSmallMode }) => (isSmallMode ? 'row' : 'column')};
+	background-color: #bbe1fa;
+	min-width: 200px;
+	border-radius: 10px;
+	cursor: pointer;
 
-    a {
-        display: flex;
-        flex-direction: ${({ isSmallMode }) =>
-            isSmallMode ? 'row' : 'column'};
-        align-items: ${({ isSmallMode }) =>
-            isSmallMode ? 'center' : 'flex-start'};
-        padding: 30px 20px;
-    }
+	a {
+		display: flex;
+		flex-direction: ${({ isSmallMode }) => (isSmallMode ? 'row' : 'column')};
+		align-items: ${({ isSmallMode }) =>
+			isSmallMode ? 'center' : 'flex-start'};
+		padding: 30px 20px 30px 20px;
+	}
 
-    @media (max-width: 900px) {
-        max-width: 100%;
-        min-width: 100%;
-        margin: 0;
-    }
+	@media (max-width: 900px) {
+		max-width: 100%;
+		min-width: 100%;
+		margin: 0;
+	}
 `;
 
 const CardChildContainer = styled.div`
-    width: 100%;
-    height: 100%;
+	width: 100%;
+	height: 100%;
 `;
 
 const Card = (props) => {
-    const { isSmallMode, children } = props;
+	const { isSmallMode, children } = props;
 
-    return (
-        <>
-            <CardParent isSmallMode={isSmallMode}>
-                <CardChildContainer>{children}</CardChildContainer>
-            </CardParent>
-        </>
-    );
+	return (
+		<>
+			<CardParent isSmallMode={isSmallMode}>
+				<CardChildContainer>{children}</CardChildContainer>
+			</CardParent>
+		</>
+	);
 };
 
 export default Card;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,20 +5,24 @@ import styled from 'styled-components';
 import Navbar from './Navbar';
 
 const LayoutDiv = styled.div`
-    margin: 0px 150px;
+	margin: 0px 150px;
 
-    @media (max-width: 1100px) {
-        margin: 0 10px;
-    }
+	@media screen and (max-width: 1024px) {
+		margin: 0 100px;
+	}
+
+	@media screen and (max-width: 820px) {
+		margin: 0 10px;
+	}
 `;
 
 const Layout = ({ children }) => {
-    return (
-        <LayoutDiv>
-            <Navbar />
-            {children}
-        </LayoutDiv>
-    );
+	return (
+		<LayoutDiv>
+			<Navbar />
+			{children}
+		</LayoutDiv>
+	);
 };
 
 export default Layout;
