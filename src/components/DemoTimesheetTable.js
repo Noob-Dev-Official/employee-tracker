a801aba950443b6a4e02e873d9200b70ccb6f150
Muntasir2001 <muntasirenamalhuda@gmail.com>
2021-10-26 16:21:11 +0100

    added add button function to add rows in the table

--- src/components/DemoTimesheetTable.js
+++ src/components/DemoTimesheetTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styled from 'styled-components';
 
@@ -6,34 +6,46 @@ import DemoTimesheetTableRow from './DemoTimesheetTableRow';
 import { Table, TableHead, TableBody } from './DemoTableComponents';
 
 const TimesheetTableParentDiv = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: flex-end;
-	margin-top: 10px;
+   display: flex;
+   flex-direction: column;
+   justify-content: flex-end;
+   margin-top: 10px;
 `;
 
 const DemoTimesheetTable = () => {
-	return (
-		<>
-			<TimesheetTableParentDiv>
-				<Table>
-					<TableHead>
-						<tr>
-							<th>Day</th>
-							<th>Month</th>
-							<th>Rate</th>
-							<th>Hour</th>
-							<th>Total</th>
-						</tr>
-					</TableHead>
-					<TableBody>
-						<DemoTimesheetTableRow TimesheetTableId={123} />
-						<button className='add-row'>Add</button>
-					</TableBody>
-				</Table>
-			</TimesheetTableParentDiv>
-		</>
-	);
+   const [numberOfRows, setNumberOfRows] = useState(0);
+
+   const onButtonClick = () => {
+      setNumberOfRows((prev) => prev + 1);
+   };
+
+   return (
+      <>
+         <TimesheetTableParentDiv>
+            <Table>
+               <TableHead>
+                  <tr>
+                     <th>Day</th>
+                     <th>Month</th>
+                     <th>Rate</th>
+                     <th>Hour</th>
+                     <th>Total</th>
+                  </tr>
+               </TableHead>
+               <TableBody>
+                  {/* <DemoTimesheetTableRow TimesheetTableId={123} />
+                  <DemoTimesheetTableRow TimesheetTableId={123} /> */}
+                  {Array.from(Array(numberOfRows), () => {
+                     return <DemoTimesheetTableRow TimesheetTableId={123} />;
+                  })}
+                  <button className='add-row' onClick={onButtonClick}>
+                     Add
+                  </button>
+               </TableBody>
+            </Table>
+         </TimesheetTableParentDiv>
+      </>
+   );
 };
 
 export default DemoTimesheetTable;
