00bb2e866c839f721a96bb80d655725a06639d3a
Muntasir <muntasirenamalhuda@gmail.com>
2021-11-06 17:10:19 +0000

    setting up redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+
+import { Provider } from 'react-redux';
+
 import App from './App';
+import './index.scss';
 
 ReactDOM.render(
 	<React.StrictMode>
---
+++ src/redux/ducks/Timesheet.js
@@ -0,0 +1,15 @@
+/* state */
+const initialState = {
+	timesheet: {
+		id: 0,
+		name: '',
+		emp_id: '',
+		timesheet_id: '',
+		rate: '',
+		hours: '',
+		total: '',
+	},
+	timesheet_num_row: {
+		num: 0,
+	},
+};
