020f7eac5ad67cc58e74a97fd474d26bebf598e4
Muntasir2001 <muntasirenamalhuda@gmail.com>
2021-11-19 18:30:22 +0000

    testing redux

--- src/components/DemoTimesheetTable.js
+++ src/components/DemoTimesheetTable.js
@@ -9,76 +9,76 @@ import { Table, TableHead, TableBody } from './DemoTableComponents';
 import { SmallTableButton } from './global/ExportedStylings';
 
 const TimesheetTableParentDiv = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: flex-end;
-	margin-top: 10px;
-	background-color: #fff;
+   display: flex;
+   flex-direction: column;
+   justify-content: flex-end;
+   margin-top: 10px;
+   background-color: #fff;
 `;
 
 const DemoTimesheetTable = () => {
-	// const [numberOfRows, setNumberOfRows] = useState(0);
+   // const [numberOfRows, setNumberOfRows] = useState(0);
 
-	const numberOfRows = useSelector(
-		(state) => state.timesheet.timesheet_table_row_count.num,
-	);
+   const numberOfRows = useSelector(
+      (state) => state.timesheet.timesheet_table_row_count.num
+   );
 
-	const dispatch = useDispatch();
+   const dispatch = useDispatch();
 
-	const onButtonClick = () => {
-		// setNumberOfRows((prev) => prev + 1);
-		dispatch(addTimesheetTableRowCount());
-	};
+   const onAddButtonClick = () => {
+      // setNumberOfRows((prev) => prev + 1);
+      dispatch(addTimesheetTableRowCount());
+   };
 
-	console.log(numberOfRows);
+   console.log(numberOfRows);
 
-	return (
-		<>
-			<TimesheetTableParentDiv>
-				<Table>
-					<TableHead padding='10px'>
-						<tr>
-							<th>Day</th>
-							<th>Month</th>
-							<th>Rate</th>
-							<th>Hour</th>
-							<th>Total</th>
-						</tr>
-					</TableHead>
-					<TableBody>
-						{/* <DemoTimesheetTableRow TimesheetTableId={123} />
+   return (
+      <>
+         <TimesheetTableParentDiv>
+            <Table>
+               <TableHead padding='10px'>
+                  <tr>
+                     <th>Day</th>
+                     <th>Month</th>
+                     <th>Rate</th>
+                     <th>Hour</th>
+                     <th>Total</th>
+                  </tr>
+               </TableHead>
+               <TableBody>
+                  {/* <DemoTimesheetTableRow TimesheetTableId={123} />
                   <DemoTimesheetTableRow TimesheetTableId={123} /> */}
-						{Array.from(Array(numberOfRows), () => {
-							return <DemoTimesheetTableRow TimesheetTableId={123} />;
-						})}
-					</TableBody>
-				</Table>
-				<div className='buttons'>
-					<SmallTableButton
-						className='add-row'
-						onClick={onButtonClick}
-						backgroundColor='#0f4c75'
-						color='#fff'
-						marginLeft='20px'
-						marginTop='15px'
-						marginBottom='10px'
-					>
-						Add
-					</SmallTableButton>
-					<SmallTableButton
-						className='update'
-						backgroundColor='#fff'
-						// color='#fff'
-						marginLeft='10px'
-						marginTop='15px'
-						marginBottom='10px'
-					>
-						Update
-					</SmallTableButton>
-				</div>
-			</TimesheetTableParentDiv>
-		</>
-	);
+                  {Array.from(Array(numberOfRows), () => {
+                     return <DemoTimesheetTableRow TimesheetTableId={123} />;
+                  })}
+               </TableBody>
+            </Table>
+            <div className='buttons'>
+               <SmallTableButton
+                  className='add-row'
+                  onClick={onAddButtonClick}
+                  backgroundColor='#0f4c75'
+                  color='#fff'
+                  marginLeft='20px'
+                  marginTop='15px'
+                  marginBottom='10px'
+               >
+                  Add
+               </SmallTableButton>
+               <SmallTableButton
+                  className='update'
+                  backgroundColor='#fff'
+                  // color='#fff'
+                  marginLeft='10px'
+                  marginTop='15px'
+                  marginBottom='10px'
+               >
+                  Update
+               </SmallTableButton>
+            </div>
+         </TimesheetTableParentDiv>
+      </>
+   );
 };
 
 export default DemoTimesheetTable;
--- src/redux/ducks/TimesheetRow.js
+++ src/redux/ducks/TimesheetRow.js
@@ -3,41 +3,79 @@ import { v4 as uuidV4 } from 'uuid';
 
 /* state */
 const initialState = {
-	timesheet_row: {
-		id: 0,
-		day: 0,
-		month: 0,
-		year: 0,
-		rate: 0,
-		total: 0,
-		timesheet_id: 0, // this could be UID
-	},
-	timesheet_row_count: {
-		num: 0,
-	},
+   timesheet_row: {
+      id: 0,
+      day: 0,
+      month: 0,
+      year: 0,
+      rate: 0,
+      hour: 0,
+      total: 0,
+      timesheet_id: 0, // this could be UID
+   },
+   timesheet_row_count: {
+      num: 0,
+   },
 };
 
 const ADD_TIMESHEET_ROW_COUNT = 'ADD_TIMESHEET_ROW_COUNT';
+const ADD_TIMESHEET_ROW = 'ADD_TIMESHEET_ROW';
 
 /* actions */
 export const addTimesheetRowCount = () => ({
-	type: ADD_TIMESHEET_ROW_COUNT,
+   type: ADD_TIMESHEET_ROW_COUNT,
+});
+
+export const addTimesheet = (
+   id,
+   day,
+   month,
+   year,
+   rate,
+   timesheet_id,
+   total
+) => ({
+   type: ADD_TIMESHEET_ROW,
+   id,
+   day,
+   month,
+   year,
+   rate,
+   total,
+   timesheet_id,
 });
 
 /* reducer */
 const timesheetRowReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case ADD_TIMESHEET_ROW_COUNT: {
-			return {
-				...state,
-				timesheet_row_count: {
-					num: state.timesheet_row_count.num + 1,
-				},
-			};
-		}
-		default:
-			return state;
-	}
+   switch (action.type) {
+      case ADD_TIMESHEET_ROW_COUNT: {
+         return {
+            ...state,
+            timesheet_row_count: {
+               num: state.timesheet_row_count.num + 1,
+            },
+         };
+      }
+
+      case ADD_TIMESHEET_ROW: {
+         const { id, day, month, year, rate, timesheet_id, total } = action;
+
+         return {
+            ...state,
+            timesheet_row: {
+               id: id,
+               day: day,
+               month: month,
+               year: year,
+               rate: rate,
+               total: rate * total,
+               timesheet_id: timesheet_id,
+            },
+         };
+      }
+      default:
+         return state;
+   }
 };
 
 export default timesheetRowReducer;
